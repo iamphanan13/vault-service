9409b0e7e4291779924174bfb451a6bb157b468a
An Phan <phanan1309@gmail.com>
2025-12-23 20:30:37 +0700

    out nlb name

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -5,3 +5,11 @@ output "public_ip_instance" {
 # output "private_ip_instance" {
 #   value = aws_instance.vault.private_ip
 # }
+
+output "vault_nlb_name" {
+  value = aws_lb.vault_nlb.dns_name
+}
+
+output "vault_nlb_zone_id" {
+  value = aws_lb.vault_nlb.zone_id
+}
