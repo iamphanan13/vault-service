0f25c769042111fcd3ec8a12059fa3c3c8705931
An Phan <phanan1309@gmail.com>
2026-01-14 10:27:09 +0700

    add AWS and Grafana

--- packer/golden-standard-ami.pkr.hcl
+++ packer/golden-standard-ami.pkr.hcl
@@ -1,41 +1,31 @@
-packer {
-  required_plugins {
-    amazon = {
-      source  = "github.com/hashicorp/amazon"
-      version = "~> 1.2.0"
-    }
-  }
-}
-
-
-source "amazon-ebs" "ubuntu" {
-  ami_name      = "golden-vault-ami-{{timestamp}}-v1.0"
+source "amazon-ebs" "ubuntu-standard" {
+  ami_name      = "golden-vault-ami-{{timestamp}}-v1.3"
   instance_type = "t3.micro"
   region        = "ap-southeast-1"
 
-source_ami_filter {
-  filters = {
-    name                = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"
-    root-device-type    = "ebs"
-    virtualization-type = "hvm"
+  source_ami_filter {
+    filters = {
+      name                = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"
+      root-device-type    = "ebs"
+      virtualization-type = "hvm"
+    }
+    owners      = ["099720109477"]
+    most_recent = true
   }
-  owners      = ["099720109477"]
-  most_recent = true
-}
 
   tags = {
-    Name = "Golden Standard AMI for Bastion Host"
+    Name = "Golden Standard AMI for Bastion Host v1.3.1"
   }
   ssh_username = "ubuntu"
 }
 
 build {
-  sources = ["source.amazon-ebs.ubuntu"]
+  sources = ["source.amazon-ebs.ubuntu-standard"]
 
   // Install Dependencies
   provisioner "shell" {
     script = "scripts/standard-ami.sh"
   }
 
-  
+
 }
--- packer/scripts/standard-ami.sh
+++ packer/scripts/standard-ami.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
 set -euo pipefail
 
+
+VAULT_VERSION="1.21.0"
+
+
 # Update apt packages and install dependencies
 echo "> Updating apt packages and installing dependencies"
 sudo apt-get update -y 
-sudo apt-get install -y gpg lsb-release wget unzip curl jq openssl
+sudo apt-get install -y gpg lsb-release wget unzip curl jq openssl apt-transport-https 
 
 # Install Ansible
 echo "> Installing Ansible"
 sudo apt-get install -y ansible
 ansible --version
 
+# Install AWS CLI
+echo "> Installing AWS CLI"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip aws
+
+# Install Vault 
+echo "> Installing Vault ${VAULT_VERSION}"
+curl -fsSL -o vault.zip https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
+sudo unzip vault.zip -d /usr/local/bin
+sudo chmod 0755 /usr/local/bin/vault
+sudo rm -f vault.zip 
+vault -v 
+
+# Install Grafana
+echo "> Installing Grafana"
+sudo mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update -y 
+sudo apt-get install -y grafana
+# Enable and start Grafana server
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable grafana-server
 
